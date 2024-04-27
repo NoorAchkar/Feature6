faef9a18894e0afc8b140388f7e730c174580675
NoorAchkar <nachkar@nd.edu>
2024-04-26 20:48:01 -0400

    bootstrap functionalities started

--- my-app/src/Components/NavMenu/NavMenu.js
+++ my-app/src/Components/NavMenu/NavMenu.js
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
 import AuthLogout from "../../Common/Services/Auth/AuthLogout";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 // Room for Change/Improvement: Using Flexbox/Bootstrap for a better NavBar
 const NavMenu = () => (
-    <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/mealsearch">Meal Search</Link>
-            </li>
-            <li>
-                <Link to="/mealtype">Meal Type</Link>
-            </li>
-            <li>
-                <Link to="/addmeal">Add a Meal</Link>
-            </li>
-        </ul>
-        <br />
-        <AuthLogout />
-    </nav>
+  <Nav className="justify-content-end" activeKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/">Home</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link href="/mealsearch">Meal Search</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link href="/mealtype">Meal Type</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link href="/addmeal">Add a Meal</Nav.Link>
+  </Nav.Item>
+  <AuthLogout />
+</Nav>
 );
 
 export default NavMenu;
